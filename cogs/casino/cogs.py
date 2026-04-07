eb9544a3ff2cc10cebf4334918282c83754f158d
JpStar06 <henriqueeapahel@gmail.com>
2026-04-07 17:53:20 -0300

    Update cogs.py

--- cogs/casino/cogs.py
+++ cogs/casino/cogs.py
@@ -10,9 +10,39 @@ from . import embeds
 class Casino(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.get_coins = services.get_coins
-        self.add_coins = services.add_coins
         
+    async def get_coins(user_id: int) -> int:
+        pool = get_connection()
+
+        async with pool.acquire() as conn:
+            row = await conn.fetchrow(
+                "SELECT coins FROM economy WHERE user_id=$1", user_id
+            )
+
+            if not row:
+                await conn.execute(
+                    "INSERT INTO economy (user_id, coins) VALUES ($1, $2)",
+                    user_id, 0
+                )
+                return 0
+
+            return row["coins"]
+
+
+    async def add_coins(user_id: int, amount: int):
+        pool = get_connection()
+
+        async with pool.acquire() as conn:
+            await conn.execute(
+                """
+                INSERT INTO economy (user_id, coins)
+                VALUES ($1, $2)
+                ON CONFLICT (user_id)
+                DO UPDATE SET coins = economy.coins + $2
+                """,
+                user_id, amount
+            )
+
 
     casino = app_commands.Group(name="casino", description="Jogos de aposta")
 
